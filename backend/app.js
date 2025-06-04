c4c117ecde633f3c4f2a5e2d63fe5f0904537606
pavan73276 <pavankumar7327@gmail.com>
2025-06-04 18:55:43 +0530

    cors installed

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,36 @@
 import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
-import {config} from "dotenv";
+import { config } from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import messageRouter from "./router/messageRouter.js";
 import noticeRouter from "./router/noticeRouter.js";
 
 const app = express();
-config({path: "./config/config.env"});
+config({ path: "./config/config.env" });
+
+const allowedOrigins = [
+  "https://fantastic-melba-68f6b8.netlify.app/",
+  "http://localhost:5173"
+];
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, 
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
   })
 );
 
-
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/message", messageRouter);
 app.use("/notice", noticeRouter);
