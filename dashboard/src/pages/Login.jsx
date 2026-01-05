00309da744c49fb8a4d48dd1ce0cd228aa588b53
pavan73276 <pavankumar7327@gmail.com>
2026-01-06 00:27:50 +0530

    cookie_expire_change3

--- dashboard/src/pages/Login.jsx
+++ dashboard/src/pages/Login.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { clearAllUserErrors, login } from "../store/slices/userSlice.js"; 
+import { clearAllUserErrors, login } from "../store/slices/userSlice.js";
 import { toast } from "react-toastify";
 
-
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const { loading, isAuthenticated, error } = useSelector((state) => state.user);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { loading, isAuthenticated, error } = useSelector(
+    (state) => state.user
+  );
 
   const dispatch = useDispatch();
   const navigateTo = useNavigate();
@@ -25,15 +26,15 @@ const Login = () => {
 
   const handleAdminlogin = (e) => {
     e.preventDefault();
-    
-    dispatch(login({email, password, role : 'admin'}));
-  }
+
+    dispatch(login({ email, password, role: "admin" }));
+  };
 
   const handleStafflogin = (e) => {
     e.preventDefault();
-    
-    dispatch(login({email, password, role : 'staff'}));
-  }
+
+    dispatch(login({ email, password, role: "staff" }));
+  };
 
   useEffect(() => {
     if (error) {
@@ -41,17 +42,15 @@ const Login = () => {
       dispatch(clearAllUserErrors());
     }
 
-    if (isAuthenticated === 'staff') {
+    if (isAuthenticated === "staff") {
       toast.success("Staff Login successfully");
       navigateTo("/staff");
     }
-    if (isAuthenticated === 'admin') {
+    if (isAuthenticated === "admin") {
       toast.success("Admin Login successfully");
       navigateTo("/admin");
     }
-
   }, [dispatch, error, loading, isAuthenticated, navigateTo]);
-
 
   return (
     <div className="flex flex-col lg:flex-row h-screen">
@@ -60,13 +59,16 @@ const Login = () => {
         className="w-full lg:w-3/5 bg-teal-900 flex justify-center items-center relative"
         style={{
           backgroundImage: "url('/background.jpg')",
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       >
-        <div className="absolute inset-0 bg-teal-900 opacity-80"></div> {/* Overlay for fading */}
         <div className="relative text-center text-white">
-          <img src="src/assets/logo.png" alt="Bailey and Co." className="w-24 mx-auto mb-4" />
+          <img
+            src="/logo.png"
+            alt="Bailey and Co."
+            className="w-24 mx-auto mb-4"
+          />
           <h1 className="text-6xl font-semibold">Palace Regalia</h1>
         </div>
       </div>
@@ -74,9 +76,13 @@ const Login = () => {
       {/* Right Section */}
       <div className="flex-1 flex justify-center items-center bg-white">
         <div className="bg-white p-8 w-full max-w-lg rounded-xl shadow-2xl">
-          <h2 className="text-3xl lg:text-4xl font-bold mb-4 text-center text-teal-900">Welcome</h2>
-          <p className="text-gray-600 mb-6 text-center">Please login to Admin or Staff Dashboard</p>
-          
+          <h2 className="text-3xl lg:text-4xl font-bold mb-4 text-center text-teal-900">
+            Welcome
+          </h2>
+          <p className="text-gray-600 mb-6 text-center">
+            Please login to Admin or Staff Dashboard
+          </p>
+
           <input
             type="text"
             placeholder="Username"
@@ -93,10 +99,16 @@ const Login = () => {
           />
 
           <div className="flex justify-between gap-4">
-            <button onClick={handleAdminlogin} className="w-1/2 bg-teal-600 hover:bg-teal-500 text-white font-semibold py-3 rounded-lg transition">
+            <button
+              onClick={handleAdminlogin}
+              className="w-1/2 bg-teal-600 hover:bg-teal-500 text-white font-semibold py-3 rounded-lg transition"
+            >
               Admin Login
             </button>
-            <button onClick={handleStafflogin} className="w-1/2 bg-orange-600 hover:bg-orange-500 text-white font-semibold py-3 rounded-lg transition">
+            <button
+              onClick={handleStafflogin}
+              className="w-1/2 bg-orange-600 hover:bg-orange-500 text-white font-semibold py-3 rounded-lg transition"
+            >
               Staff Login
             </button>
           </div>
